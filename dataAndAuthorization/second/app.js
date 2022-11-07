e3bc4c4309c8c8ef4d2942f8df08ac73fee1ba01
SashoAlex2000 <sahooo3@abv.bg>
2022-11-07 20:02:16 +0200

    adding files

---
+++ dataAndAuthorization/second/app.js
@@ -0,0 +1,133 @@
+
+// implement the data base communication functionality first, DOM second
+// that way we ensure that the DB funcionality works and the problems 
+// are in other places
+
+// initializes DOM comm
+
+const listOfComments = document.getElementById('comments');
+const nameInput = document.querySelector('[name="name"]');
+const commentInput = document.querySelector('[name="content"]');
+
+init();
+
+function init() {
+
+    document.getElementById('load-the-comments').addEventListener('click', getComments);
+
+    document.getElementById('send').addEventListener('click', onPost);
+
+    getComments();
+
+    listOfComments.addEventListener('click', clickedComment);
+
+}
+
+
+function clickedComment(event) {
+
+    if (event.target.tagName === 'BUTTON') {
+        console.log('click');
+        let currentID = event.target.parentElement.parentElement.id
+        console.log(currentID)
+        
+        deleteComment(currentID)
+    }
+
+    
+}
+
+
+async function onPost() {
+
+    const name = nameInput.value;
+    const content = commentInput.value;
+
+    let result = await postComment({
+        name, content
+    })
+
+    listOfComments.prepend(createComment(result));
+
+    nameInput.value = '';
+    commentInput.value = '';
+
+}
+
+function displayComments(comments) {
+
+    listOfComments.replaceChildren(...comments.map(createComment))
+}
+
+function createComment(comment) {
+
+    let article = document.createElement('article');
+
+    article.innerHTML = `
+    <header>
+    <h3>${comment.name}</h3>
+    </header>
+    <main>
+    <p>${comment.content}</p>
+    <button class="delete-btn">Delete this comment</button>
+    </main>
+    `;
+
+    article.id = comment._id;
+
+    return article
+
+}
+
+
+async function getComments() {
+    
+    const response = await fetch("http://localhost:3030/jsonstore/comments");
+
+    const data = await response.json();
+
+    const comments =  Object.values(data).reverse();
+    displayComments(comments);
+    
+}
+
+async function postComment(comment) {
+
+    const response = await fetch("http://localhost:3030/jsonstore/comments", {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    })
+
+    const data = await response.json();
+
+    return data;
+
+}
+
+async function deleteComment(commentID) {
+    const response = await fetch('http://localhost:3030/jsonstore/comments/' + commentID, {
+        method: 'delete'
+    });
+
+    console.log(response)
+
+    document.getElementById(commentID).remove();
+
+}
+
+async function updateComment(commentID, comment) {
+    const response = await fetch('http://localhost:3030/jsonstore/comments/' + commentID, {
+        method: 'put',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+    })
+
+    return response.json()
+
+}
+
