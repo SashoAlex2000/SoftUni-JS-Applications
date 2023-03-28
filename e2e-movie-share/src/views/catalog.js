479dd951af9a279070b559f066a5f7ea9580d89c
SashoAlex2000 <sahooo3@abv.bg>
2023-03-28 11:33:14 +0300

    feat: added clear for seach and filtering by rating (html only for now)

--- e2e-movie-share/src/views/catalog.js
+++ e2e-movie-share/src/views/catalog.js
@@ -2,8 +2,9 @@
 import { html, nothing } from "../lib/lit-html.js";
 import { repeat } from '../lib/directives/repeat.js'
 import { getAllMovies, getMovieBySearchWord } from "../data/movie.js";
+import { movieOptions } from "../util.js";
 
-const catalogTemplate = (movies, onSearch) => html `
+const catalogTemplate = (movies, onSearch, isSearch, options) => html `
     <div class="search-bar-wrapper">
         <div class="search-div">
             <form class="search-form" @submit=${onSearch}>
@@ -11,8 +12,22 @@ const catalogTemplate = (movies, onSearch) => html `
 
                 </input>
                 <button class="meta-search-button search-button">Search</button>
+                ${isSearch ? html `
+                <a href="/catalog">Clear</a>
+                ` : nothing}
             </form>
         </div>
+    </div>
+    <div class="rating-filter-wrapper">
+        <div class="rating-filter">
+            <form>
+                <label for="rating-filter"> Choose Categories: </label>    
+                <select name="rating" id="rating-filter" multiple>
+                    ${repeat(options, optionsCard)}
+                </select>
+                <button type="submit">Filter</button>
+            </form>
+        </div>    
     </div>
     <div class="movie-wrapper">
         <!-- Throws error with a single item -->
@@ -30,17 +45,22 @@ const movieCard = (movie) => html `
     </div>
 `;
 
+const optionsCard = (option) => html `
+<option value="${option[0]}">${option[1]}</option>
+`
+
 export async function catalogView(ctx) {
 
     // a midleware was needed first ...
     console.log(`the query is : ${ctx.query.search}`);
     console.log(`the filter query is : ${ctx.query.filter}`);
     let movies;
+    let isSearch = Boolean(ctx.query.search);
     if (!!ctx.query && !!ctx.query.search) {
         const queryString = ctx.query.search;
         console.log(queryString);
 
-        const {result: moviesBySearchWord} = await getMovieBySearchWord(queryString);
+        const {results: moviesBySearchWord} = await getMovieBySearchWord(queryString);
         console.log(moviesBySearchWord);
         movies = moviesBySearchWord;
 
@@ -49,14 +69,16 @@ export async function catalogView(ctx) {
         movies = allMovies;
     }
 
+    const options = Object.entries(movieOptions);
+
     console.log(movies);
-    ctx.render(catalogTemplate(movies, onSearch));
+    ctx.render(catalogTemplate(movies, onSearch, isSearch, options));
 
     async function onSearch (event) {
         event.preventDefault();
         const searchedWord = event.target.parentElement.querySelector('input');
 
-        ctx.page.redirect(`/catalog/?search=${searchedWord.value}`);
+        ctx.page.redirect(`/catalog?search=${searchedWord.value}`);
 
     }
 
